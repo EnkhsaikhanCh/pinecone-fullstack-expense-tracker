6c94a3ee33ba5c4ddd729e8b7abcd32f06e55e96
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-29 20:31:52 +0800

    Connected - Transactions from API to UI

--- expense-ui/components/Card.jsx
+++ expense-ui/components/Card.jsx
@@ -1,8 +1,51 @@
+import axios from "axios";
+
 import { GrHomeRounded } from "react-icons/gr";
 import { Header } from "./Header";
 import { AddRecordButton } from "./AddRecordButton";
+import { useEffect, useState } from "react";
+
+const API_BASE_URL = `http://localhost:3000/transactions`;
+// const CREATE_ENDPOINT = `create`;
+// const UPDATE_ENDPOINT = `update`;
+// const DELETE_ENDPOINT = `delete`;
 
 export function Card() {
+  const [transactions, setTransactions] = useState([]);
+
+  function loadTasks() {
+    axios.get(`${API_BASE_URL}`).then(
+      (response) => {
+        setTransactions(response.data);
+      },
+      (error) => {
+        console.error("Error loading task:", error.message);
+      },
+    );
+  }
+
+  // Read ---------------------------------------------
+  useEffect(() => {
+    loadTasks();
+  }, []);
+
+  // Create ---------------------------------------------
+  function createTransaction() {
+    const title = document.getElementById("title");
+    const amount = document.getElementById("amount");
+    const description = document.getElementById("description");
+
+    if ((title, amount, description)) {
+      axios
+        .post(`${API_BASE_URL}/${CREATE_ENDPOINT}`, {
+          title,
+        })
+        .then(() => {
+          loadTasks();
+        });
+    }
+  }
+
   return (
     <>
       <Header />
@@ -10,45 +53,36 @@ export function Card() {
         <div>
           <FilterSection />
         </div>
-        <div className="card w-full bg-neutral">
-          <div className="flex justify-between py-4 pr-4">
-            <div className="flex">
-              <div className="flex w-14 items-center justify-center">
-                <GrHomeRounded />
+        <div className="card flex w-full gap-2 ">
+          {transactions.map((transaction) => (
+            <div
+              key={transaction.id}
+              className="flex justify-between rounded-md bg-neutral py-4 pr-4"
+            >
+              <div className="flex">
+                <div className="flex w-14 items-center justify-center">
+                  <GrHomeRounded />
+                </div>
+                <div>
+                  <h1 className="text-lg">{transaction.title}</h1>
+                  <p className="text-sm">{transaction.description}</p>
+                </div>
               </div>
-              <div>
-                <h1 className="text-lg">Lending & Renting</h1>
-                <p className="text-sm">14:00</p>
+              <div className="flex items-center gap-2 text-lg">
+                <p>{transaction.amount}</p>
+                <button className="btn">Edit</button>
               </div>
             </div>
-            <div className="flex items-center text-lg">
-              <p>- 1000</p>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
   );
 }
 
-// const AddRecord = () => {
-//   function addRecord() {
-//     alert("Hello");
-//   }
-
-//   return (
-//     <div>
-//       <button className="btn btn-primary" onClick={addRecord}>
-//         <IoAddOutline />
-//         Add
-//       </button>
-//     </div>
-//   );
-// };
-
 const FilterSection = () => {
   return (
-    <div className="card h-full w-[300px] bg-neutral px-4 pt-2">
+    <div className="card h-full w-[300px] rounded-md bg-neutral px-4 pt-2">
       <AddRecordButton />
     </div>
   );
