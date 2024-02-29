4ae482a00e6bb772b74e827b2ef31acef6b5089c
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-02-29 20:28:13 +0800

    CRUD - Transactions

--- expense-api/controller/transactions.controller.js
+++ expense-api/controller/transactions.controller.js
@@ -1,29 +1,41 @@
-const postgres = require("postgres");
-require("dotenv").config();
-
-let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
-
-const sql = postgres({
-  host: PGHOST,
-  database: PGDATABASE,
-  username: PGUSER,
-  password: PGPASSWORD,
-  port: 5432,
-  ssl: "require",
-  connection: {
-    options: `project=${ENDPOINT_ID}`,
-  },
-});
+const { sql } = require("../config/database");
 
 // Create ---------------------------------------------
-const createTransactions = async (req, res) => {
-  const { title, amount, transcation_type, description } = req.body;
+const createTransaction = async (req, res) => {
+  const { title, amount, description } = req.body;
   const response =
-    await sql`insert into users(title, amount, transcation_type, description) values(${title}, ${amount}, ${transcation_type}, ${description})`;
+    await sql`insert into transactions(title, amount, description) values(${title}, ${amount}, ${description})`;
 
   res.json(response);
 };
 
+// Read ---------------------------------------------
+const getTransaction = async (req, res) => {
+  const result = await sql`select * from transactions`;
+  res.json(result);
+};
+
+// Update ---------------------------------------------
+const updateTransaction = async (req, res) => {
+  const { id } = req.params;
+  const { title, amount, description } = req.body;
+
+  const result =
+    await sql`UPDATE transactions SET title = ${title}, amount = ${amount}, description = ${description} WHERE transaction_id = ${id}`;
+  res.json(result);
+};
+
+// Delete ---------------------------------------------
+const deleteTransaction = async (req, res) => {
+  const { id } = req.params;
+  const result =
+    await sql`delete from transactions where transaction_id = ${id}`;
+  res.json(result);
+};
+
 module.exports = {
-  createTransactions,
+  createTransaction,
+  getTransaction,
+  updateTransaction,
+  deleteTransaction,
 };
--- expense-api/router/transactions.router.js
+++ expense-api/router/transactions.router.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
-const { createTransactions } = require("../controller/transactions.controller");
+const {
+  createTransaction,
+  getTransaction,
+  updateTransaction,
+  deleteTransaction,
+} = require("../controller/transactions.controller");
 
-const usersRouter = express.Router();
+const transactionsRouter = express.Router();
 
-usersRouter.post("/create", createTransactions);
+transactionsRouter.post("/create", createTransaction);
+transactionsRouter.get("/", getTransaction);
+transactionsRouter.put("/update/:id", updateTransaction);
+transactionsRouter.delete("/delete/:id", deleteTransaction);
 
-module.exports = usersRouter;
+module.exports = transactionsRouter;
