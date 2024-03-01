f2628d2831101cf7a34bb52e96b1a4fdf5208323
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-01 11:11:41 +0800

    Fixed - useState for create transaction

--- expense-ui/components/AddRecordButton.jsx
+++ expense-ui/components/AddRecordButton.jsx
@@ -1,50 +1,52 @@
-// import axios from "axios";
 import axios from "axios";
-
 import { useEffect, useState } from "react";
 import { IoAddOutline } from "react-icons/io5";
 
 const API_BASE_URL = `http://localhost:3000/transactions`;
 const CREATE_ENDPOINT = `create`;
-// const UPDATE_ENDPOINT = `update`;
-// const DELETE_ENDPOINT = `delete`;
-
-function refreshPage() {
-  window.location.reload(false);
-}
 
 export function AddRecordButton() {
   const [transactions, setTransactions] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    amount: "",
+    description: "",
+  });
 
-  function loadTransactions() {
-    axios.get(`${API_BASE_URL}`).then(
-      (response) => {
-        setTransactions(response.data);
-      },
-      (error) => {
-        console.error("Error loading task:", error.message);
-      },
-    );
+  // Function to load transactions from the server
+  async function loadTransactions() {
+    try {
+      const response = await axios.get(API_BASE_URL);
+      setTransactions(response.data);
+    } catch (error) {
+      console.error("Error loading transactions:", error.message);
+    }
   }
 
-  function createTransaction() {
-    const title = document.getElementById("title").value;
-    const amount = document.getElementById("amount").value;
-    const description = document.getElementById("description").value;
-
-    console.log({ title, amount, description });
-
-    if (title && amount && description) {
-      axios
-        .post(`${API_BASE_URL}/${CREATE_ENDPOINT}`, {
+  async function createTransaction() {
+    try {
+      const { title, amount, description } = formData;
+      if (title && amount && description) {
+        await axios.post(`${API_BASE_URL}/${CREATE_ENDPOINT}`, {
           title,
           amount,
           description,
-        })
-        .then(() => {
-          loadTransactions();
-          refreshPage();
         });
+
+        // Clear form data after successful submission
+        setFormData({
+          title: "",
+          amount: "",
+          description: "",
+        });
+
+        closeModal();
+        loadTransactions();
+      } else {
+        console.error("Invalid input values. Please check your inputs.");
+      }
+    } catch (error) {
+      console.error("Error creating transaction:", error.message);
     }
   }
 
@@ -52,12 +54,24 @@ export function AddRecordButton() {
     loadTransactions();
   }, []);
 
+  const handleInputChange = (input) => {
+    setFormData({
+      ...formData,
+      [input.target.id]: input.target.value,
+    });
+  };
+
+  const openModal = () => {
+    document.getElementById("my_modal_1").showModal();
+  };
+
+  const closeModal = () => {
+    document.getElementById("my_modal_1").close();
+  };
+
   return (
     <div>
-      <button
-        className="btn"
-        onClick={() => document.getElementById("my_modal_1").showModal()}
-      >
+      <button className="btn" onClick={openModal}>
         <IoAddOutline />
         Add record
       </button>
@@ -73,12 +87,16 @@ export function AddRecordButton() {
               placeholder="Title"
               id="title"
               className="input input-bordered w-full max-w-xs"
+              value={formData.title}
+              onChange={handleInputChange}
             />
             <input
               type="text"
               placeholder="Amount"
               id="amount"
               className="input input-bordered w-full max-w-xs"
+              value={formData.amount}
+              onChange={handleInputChange}
             />
             <label className="form-control">
               <div className="label">
@@ -88,13 +106,18 @@ export function AddRecordButton() {
                 className="textarea textarea-bordered h-24"
                 id="description"
                 placeholder="Write here"
+                value={formData.description}
+                onChange={handleInputChange}
               ></textarea>
             </label>
           </div>
           <div className="modal-action">
-            <form method="dialog">
+            <form method="dialog" className="flex gap-2">
               <button className="btn" onClick={createTransaction}>
                 Done
+              </button>
+              <button className="btn" onClick={closeModal}>
+                Close
               </button>
             </form>
           </div>
