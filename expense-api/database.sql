28824c487452db8ea4eb2b07ed66cec10e22f11a
Enkhsaikhan <ch.enhsaihan@gmail.com>
2024-03-04 20:09:41 +0800

    Update - Database sql

--- expense-api/database.sql
+++ expense-api/database.sql
@@ -8,3 +8,45 @@ CREATE TABLE users (
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   currency_type TEXT DEFAULT 'MNT'
 )
+
+-- CREATE TABLE categories (
+--     id VARCHAR(21) PRIMARY KEY,
+--     name VARCHAR(55)
+-- )
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(55)
+)
+
+-- CREATE TABLE transactions (
+--     id VARCHAR(21) PRIMARY KEY,
+--     amount MONEY,
+--     date TIMESTAMP,
+--     category_id VARCHAR(21),
+--     CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id)
+-- )
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    amount MONEY,
+    date TIMESTAMP,
+    category_id SERIAL,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id)
+)
+
+-- Categories
+INSERT INTO categories VALUES('1', 'Food & Drinks');
+INSERT INTO categories VALUES('2', 'Housing');
+INSERT INTO categories VALUES('3', 'Shopping');
+
+-- Transactions
+INSERT INTO transactions(id, amount, category_id) VALUES('12', 10000, '1');
+INSERT INTO transactions(id, amount, category_id) VALUES('14', 23000, '2');
+INSERT INTO transactions(id, amount, category_id) VALUES('18', -5000, '3');
+-- After change
+INSERT INTO transactions(amount, category_id) VALUES(15000, '3');
+
+
+-- 
+SELECT transactions.id, amount, category_id, categories.name category_name FROM transactions LEFT JOIN categories on transactions.category_id = categories.id;
