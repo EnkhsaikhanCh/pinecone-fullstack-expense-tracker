0c6c0b27dd6acbcb29bb6bff37da740643919b16
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-03-16 22:31:52 +0800

    Connected DB

--- expense-ui/components/widgets/CashWidget.jsx
+++ expense-ui/components/widgets/CashWidget.jsx
@@ -1,7 +1,26 @@
+import axios from "axios";
 import { LogoSVG } from "../image/LogoSVG";
 import { ChipSVG } from "../image/ChipSVG";
+import { useEffect, useState } from "react";
 
 export function CashWidget() {
+  const [balance, setBalance] = useState([]);
+
+  async function getNetBalance() {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/transactions/balance`,
+      );
+      setBalance(response.data.netBalance);
+    } catch (error) {
+      console.error("Failed to fetch net balance:", error);
+    }
+  }
+
+  useEffect(() => {
+    getNetBalance();
+  }, []);
+
   return (
     <div className="card h-[200px] w-[384px] bg-neutral text-white shadow-sm">
       <div className="card-body flex justify-between">
@@ -11,8 +30,8 @@ export function CashWidget() {
         </div>
         <div className="flex items-center justify-between">
           <div>
-            <p className="text-gray-400">Cash</p>
-            <p className="text-2xl">10,000</p>
+            <p className="text-gray-400">Balance</p>
+            {balance !== null && <p className="text-2xl">{balance}</p>}
           </div>
           <div>
             <ChipSVG />
