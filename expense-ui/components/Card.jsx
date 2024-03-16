baf440f3bb0b3f5407487c682eb97eb56a6a92bd
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-03-16 22:28:03 +0800

    Updated - Transaction conditional and UI

--- expense-ui/components/Card.jsx
+++ expense-ui/components/Card.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr"; // Ensure mutate is imported correctly
 import { GrHomeRounded } from "react-icons/gr";
 import { PiTrashDuotone } from "react-icons/pi";
 import dayjs from "dayjs";
@@ -8,53 +8,84 @@ const API_BASE_URL = `http://localhost:3000/transactions`;
 const DELETE_ENDPOINT = `delete`;
 
 export function Card() {
-  const { data: transactions } = useSWR(API_BASE_URL, fetcher);
+  const { data: transactions, error } = useSWR(API_BASE_URL, fetcher);
+  const isLoading = !transactions && !error;
 
   async function fetcher(url) {
-    const response = await axios.get(url);
-    return response.data;
+    try {
+      const response = await axios.get(url);
+      return response.data;
+    } catch (error) {
+      throw new Error("Failed to fetch transactions");
+    }
   }
 
-  // Delete ---------------------------------------------
   function deleteTransaction(transaction_id) {
     if (window.confirm("Delete?")) {
       axios
         .delete(`${API_BASE_URL}/${DELETE_ENDPOINT}/${transaction_id}`)
-        .then(() => {});
+        .then(() => {
+          mutate(API_BASE_URL);
+        });
     }
+  }
+
+  function parseAmount(amountString) {
+    const numericValue = Number(amountString.replace(/[^0-9.-]+/g, ""));
+    return numericValue;
+  }
+
+  function getAmountClassName(amount) {
+    const amountNumber = parseAmount(amount);
+    return amountNumber < 0
+      ? "rounded-md border border-red-300 bg-rose-100 px-3 font-semibold"
+      : "rounded-md border border-emerald-300 bg-emerald-100 px-3 font-semibold";
+  }
+
+  if (isLoading) {
+    return <CardSkeleton />;
+  }
+
+  if (error) {
+    return <div>Error loading transactions.</div>;
+  }
+
+  if (Array.isArray(transactions) && transactions.length === 0) {
+    return <div>No transactions found.</div>;
   }
 
   return (
     <>
       <div className="flex w-full flex-col items-center justify-between gap-3 place-self-start md:flex-row lg:w-[1000px]">
         <div className="card flex w-full gap-2 ">
-          {Array.isArray(transactions) && transactions.length > 0
-            ? transactions.map((transaction) => (
-                <div
-                  key={transaction.id}
-                  className="flex justify-between rounded-md bg-white py-4 pr-4"
-                >
-                  <div className="flex">
-                    <div className="flex w-14 items-center justify-center">
-                      <GrHomeRounded />
-                    </div>
-                    <div>
-                      <h1 className="text-lg">{transaction.category_name}</h1>
-                      <Date transaction={transaction} />
-                    </div>
-                  </div>
-                  <div className="flex items-center gap-2 text-lg">
-                    <p>{transaction.amount}</p>
-                    <button
-                      className="btn h-[48px] w-[48px] px-[10px]"
-                      onClick={() => deleteTransaction(transaction.id)}
-                    >
-                      <PiTrashDuotone className="h-[20px] w-[20px]" />
-                    </button>
-                  </div>
+          {transactions.map((transaction) => (
+            <div
+              key={transaction.id}
+              className="flex justify-between rounded-md bg-white py-4 pr-4"
+            >
+              <div className="flex">
+                <div className="flex w-14 items-center justify-center">
+                  <GrHomeRounded />
                 </div>
-              ))
-            : CardSkeleton()}
+                <div>
+                  <h1 className="text-lg">{transaction.category_name}</h1>
+                  <Date transaction={transaction} />
+                </div>
+              </div>
+              <div className="flex items-center gap-2 text-lg">
+                <span className={getAmountClassName(transaction.amount)}>
+                  {transaction.amount}
+                </span>
+
+                <button
+                  className="btn h-[48px] w-[48px] px-[10px]"
+                  onClick={() => deleteTransaction(transaction.id)}
+                >
+                  <PiTrashDuotone className="h-[20px] w-[20px]" />
+                </button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
@@ -63,7 +94,6 @@ export function Card() {
 
 function Date({ transaction }) {
   const date = dayjs(transaction.date).format("HH:mm");
-  console.log(date);
   return <p className="text-sm text-[#6B7280]">{date}</p>;
 }
 
