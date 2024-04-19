7ecac9bbda6cc3fc7bc12bee0d52166fb5a42199
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-04-19 18:20:29 +0800

    fix

--- expense-api/router/users.router.js
+++ expense-api/router/users.router.js
@@ -11,7 +11,7 @@ const { checkAuth } = require("../middleware/checkAuth");
 const usersRouter = express.Router();
 
 usersRouter.post("/signUp", createUser);
-usersRouter.post("/login", checkAuth, getUser);
+usersRouter.post("/login", getUser);
 usersRouter.put("/update/:id", updateUser);
 usersRouter.get("/", listUsers);
 usersRouter.get("/checkUsername", checkUsername);
