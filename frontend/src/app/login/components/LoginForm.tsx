95e5f752381187b28d33f5ccd645e7760c541e0a
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-04-20 12:43:35 +0800

    Create - Login

---
+++ frontend/src/app/login/components/LoginForm.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { Toaster, toast } from "sonner";
+import { useState } from "react";
+import { Field } from "@/components/Field";
+import { PasswordField } from "@/components/PasswordField";
+import { Mutator } from "@/app/utils";
+import { AuthBtnSmall } from "@/components/AuthBtnSmall";
+
+interface ApiResponseError {
+  response?: {
+    data?: {
+      message?: string;
+    };
+  };
+}
+
+export function LoginForm() {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
+  const validateForm = () => {
+    if (!username || !email || !password) {
+      toast.warning("All fields are required");
+      return false;
+    }
+
+    return true;
+  };
+
+  const LoginSubmit = async () => {
+    if (!validateForm()) return;
+
+    try {
+      const loginPromise = Mutator("users/login", {
+        username,
+        email,
+        password,
+      });
+
+      toast.promise(loginPromise, {
+        loading: "Logging in...",
+        success: (accessToken) => {
+          localStorage.setItem("accessToken", accessToken);
+          setTimeout(() => {
+            window.location.href = "/";
+          }, 1000);
+          return "Logged in successfully!";
+        },
+        error: (error: ApiResponseError) => {
+          const message =
+            error.response?.data?.message || "Error during login.";
+          return message;
+        },
+      });
+    } catch (error) {
+      console.error("Login error:", error);
+    }
+  };
+
+  const togglePasswordVisibility = () => {
+    setPasswordVisible(!passwordVisible);
+  };
+
+  return (
+    <div className="card w-full rounded-md border bg-white">
+      <Toaster position="top-center" richColors />
+      <div className="card-body px-[20px] py-4">
+        <div className="flex flex-col gap-3">
+          {/* Username */}
+          <Field
+            label="Username"
+            type="text"
+            value={username}
+            onChange={setUsername}
+          />
+
+          {/* Email */}
+          <Field label="Email" type="email" value={email} onChange={setEmail} />
+
+          {/* Password */}
+          <PasswordField
+            label="Password"
+            visible={passwordVisible}
+            value={password}
+            onChange={setPassword}
+            toggleVisibility={togglePasswordVisibility}
+            showForgotPassword={true}
+          />
+        </div>
+
+        {/* Login button */}
+        <AuthBtnSmall label="Log in" onClick={LoginSubmit} />
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,15 @@
+import { AuthHeader } from "@/components/AuthHeader";
+import { AuthFooter } from "@/components/AuthFooter";
+import { LoginForm } from "./components/LoginForm";
+
+export default function Home() {
+  return (
+    <main>
+      <div className="container mx-auto mt-10 flex w-[342px] flex-col items-center gap-3 px-4">
+        <AuthHeader label="Welcome Back" />
+        <LoginForm />
+        <AuthFooter label="Sign up" href="/signUp" text="Don't have account?" />
+      </div>
+    </main>
+  );
+}
