a15709a24e06f7fc2d630195d7b15a9b16b82209
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-04-23 20:07:01 +0800

    Create - Header

---
+++ frontend/src/components/header/header.tsx
@@ -0,0 +1,52 @@
+import { LogoSVG } from "../image/LogoSVG";
+
+export function Header() {
+  return (
+    <div className="bg-white">
+      <div className="container mx-auto">
+        <div className="navbar">
+          <div className="flex flex-1 gap-2">
+            <LogoSVG />
+            <a href="/dashboard" className="btn btn-ghost text-lg">
+              Dashboard
+            </a>
+            <a href="/records" className="btn btn-ghost text-lg">
+              Records
+            </a>
+          </div>
+          <div className="dropdown dropdown-end">
+            <div
+              tabIndex={0}
+              role="button"
+              className="avatar btn btn-circle btn-ghost"
+            >
+              <div className="w-10 rounded-full">
+                <img
+                  alt="Tailwind CSS Navbar component"
+                  src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                />
+              </div>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu dropdown-content menu-sm z-[1] mt-3 w-52 rounded-md bg-base-100 p-2 shadow"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
