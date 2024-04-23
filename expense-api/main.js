59efd1dcfdb7fd18ad1d942645a7ce10004293a3
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-04-23 20:05:00 +0800

    fix

--- expense-api/main.js
+++ expense-api/main.js
@@ -5,7 +5,7 @@ const transactionsRouter = require("./router/transactions.router");
 const categoriesRouter = require("./router/categories.router");
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.json());
 app.use(cors());
