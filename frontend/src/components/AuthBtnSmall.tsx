a3b9c6915078fc447dd08969388279e20575ef1f
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-04-28 23:40:04 +0800

    Update - Disabled and Arrow icon condition

--- frontend/src/components/AuthBtnSmall.tsx
+++ frontend/src/components/AuthBtnSmall.tsx
@@ -1,16 +1,24 @@
+import { FaArrowRightLong } from "react-icons/fa6";
+
 export function AuthBtnSmall({
   label,
   onClick,
+  disabled,
+  showArrow,
 }: {
   label: string;
   onClick: () => void;
+  disabled?: boolean;
+  showArrow?: boolean;
 }) {
   return (
     <button
-      className="btn btn-neutral btn-sm mt-2 rounded-[5px] text-white"
+      className="btn btn-neutral btn-sm w-full rounded-[5px] text-white"
       onClick={onClick}
+      disabled={disabled}
     >
       {label}
+      {showArrow && <FaArrowRightLong />}
     </button>
   );
 }
