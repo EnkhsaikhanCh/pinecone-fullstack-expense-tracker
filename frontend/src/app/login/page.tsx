b7c6fc4d9481e64688cbe65c34aa81f5dada342e
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-05-28 21:43:23 +0800

    Add auth checker and ProtectedRoute

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -1,8 +1,27 @@
+"use client";
+
 import { AuthHeader } from "@/components/AuthHeader";
 import { AuthFooter } from "@/components/AuthFooter";
 import { LoginForm } from "./components/LoginForm";
+import { useAuthStore } from "@/store/authStore";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { PageLoading } from "@/components/PageLoading";
 
-export default function Home() {
+export default function Login() {
+  const { authStatus } = useAuthStore();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (authStatus === "isAuth") {
+      router.push("/dashboard");
+    }
+  }, [authStatus, router]);
+
+  if (authStatus === "unknownAuth") {
+    return <PageLoading />;
+  }
+
   return (
     <main>
       <div className="container mx-auto mt-10 flex w-[342px] flex-col items-center gap-3 px-4">
