d174a18b2f125478fd5cb908b6a5b7dc4bb0a618
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-05-28 21:38:15 +0800

    Create - PageLoading

---
+++ frontend/src/components/PageLoading.tsx
@@ -0,0 +1,7 @@
+export function PageLoading() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <span className="loading loading-spinner loading-lg text-[#3498db]"></span>
+    </div>
+  );
+}
