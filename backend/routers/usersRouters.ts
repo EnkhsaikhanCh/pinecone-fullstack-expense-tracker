4b1246c3746e58bdf21c8858d6873e3efb7b56f3
Enkhsaikhan <99777231+EnkhsaikhanCh@users.noreply.github.com>
2024-06-26 20:42:56 +0800

    delete mongo backend and frontend

--- backend/routers/usersRouters.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { createUser } from "../controllers/usersControllers";
-import { validateUser } from "../middleware/userValidation";
-
-const usersRouter = express.Router();
-
-usersRouter.post("/register", validateUser, createUser);
-
-export default usersRouter;
--- frontend/src/components/PageLoading.tsx
+++
@@ -1,7 +0,0 @@
-export function PageLoading() {
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <span className="loading loading-spinner loading-lg text-[#3498db]"></span>
-    </div>
-  );
-}
